0cf1d2f34f72e9e2b844fc7eea9de63f6b710e79
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2022-03-18 16:57:52 +0000

    chore: Add fonts and initial typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue,md}'],
+
+	theme: {
+		fontFamily: {
+            'sans': ['"Open Sans"', ...defaultTheme.fontFamily.sans],
+            'serif': [...defaultTheme.fontFamily.serif],
+            'mono': [...defaultTheme.fontFamily.mono]
+        },
+	}
 };
