236fbc6b5185b33851cbf0ae90f9291367950a73
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2022-03-21 17:21:18 +0000

    chore: add site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
 	// Enable the Preact renderer to support Preact JSX components.
 	renderers: ['@astrojs/renderer-vue'],
+	buildOptions: {
+		site: 'https://design-system-spec-lloyd.netlify.app'
+	}
 });
