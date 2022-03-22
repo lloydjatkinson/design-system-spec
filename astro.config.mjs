fc78f9de6d43134849daebdf84aa8db8156c5e5b
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2022-03-22 15:05:42 +0000

    chore: typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable the Preact renderer to support Preact JSX components.
 	renderers: ['@astrojs/renderer-vue'],
 	buildOptions: {
 		site: 'https://design-system-spec-lloyd.netlify.app',
--- src/components/design/PaddingMarginPlayground.vue
+++ src/components/design/PaddingMarginPlayground.vue
@@ -53,46 +53,6 @@ import clsx from 'clsx';
 
 import { Gap, GridColumns, Padding, Margin } from './design-tokens';
 
-type GapTokens = [ gap: Gap, gapX: Gap, gapY: Gap ];
-const availableGapTokens: GapTokens[] = [
-    ['gap-0', 'gap-x-0', 'gap-y-0'],
-    ['gap-0.5', 'gap-x-0.5', 'gap-y-0.5'],
-    ['gap-1', 'gap-x-1', 'gap-y-1'],
-    ['gap-1.5', 'gap-x-1.5', 'gap-y-1.5'],
-    ['gap-2', 'gap-x-2', 'gap-y-2'],
-    ['gap-2.5', 'gap-x-2.5', 'gap-y-2.5'],
-    ['gap-3', 'gap-x-3', 'gap-y-3'],
-    ['gap-3.5', 'gap-x-3.5', 'gap-y-3.5'],
-    ['gap-4', 'gap-x-4', 'gap-y-4'],
-    ['gap-5', 'gap-x-5', 'gap-y-5'],
-    ['gap-6', 'gap-x-6', 'gap-y-6'],
-    ['gap-7', 'gap-x-7', 'gap-y-7'],
-    ['gap-8', 'gap-x-8', 'gap-y-8'],
-    ['gap-10', 'gap-x-10', 'gap-y-10'],
-    ['gap-11', 'gap-x-11', 'gap-y-11'],
-    ['gap-12', 'gap-x-12', 'gap-y-12'],
-    ['gap-14', 'gap-x-14', 'gap-y-14'],
-    ['gap-16', 'gap-x-16', 'gap-y-16'],
-    ['gap-20', 'gap-x-20', 'gap-y-20'],
-    ['gap-24', 'gap-x-24', 'gap-y-24'],
-    ['gap-28', 'gap-x-28', 'gap-y-28'],
-];
-
-const availiableColumnTokens: GridColumns[] = [
-    'grid-cols-1',
-    'grid-cols-2',
-    'grid-cols-3',
-    'grid-cols-4',
-    'grid-cols-5',
-    'grid-cols-6',
-    'grid-cols-7',
-    'grid-cols-8',
-    'grid-cols-9',
-    'grid-cols-10',
-    'grid-cols-11',
-    'grid-cols-12',
-];
-
 const availiablePadding: Padding[] = [
     'p-0',
     'p-0.5',
